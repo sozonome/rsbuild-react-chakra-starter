4396be56bb26f7eee5cb3871e55f5d00c31d4b10
Agustinus Nathaniel <berserk.nate@gmail.com>
2024-01-06 11:02:30 +0700

    chore: setup million auto

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -4,6 +4,7 @@ import { pluginAssetsRetry } from '@rsbuild/plugin-assets-retry';
 import { pluginReact } from '@rsbuild/plugin-react';
 import { pluginSvgr } from '@rsbuild/plugin-svgr';
 import { pluginTypeCheck } from '@rsbuild/plugin-type-check';
+import million from 'million/compiler';
 
 export default defineConfig({
   plugins: [
@@ -14,5 +15,10 @@ export default defineConfig({
   ],
   html: {
     template: './public/index.html',
+  },
+  tools: {
+    rspack: {
+      plugins: [million.webpack({ auto: true })],
+    },
   },
 });
